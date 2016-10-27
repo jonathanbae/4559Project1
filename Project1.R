fba14c340ac73f33b603ce622c76dfc903fb6b2d
jonathanbae <jonathanbae7@gmail.com>
2016-10-26 20:50:39 -0400

    upload

---
+++ Project1.R
@@ -0,0 +1,153 @@
+#read in table
+date <- read.csv("~/School/Fall 2016/DS 4559/Project 1/Speed Dating Data.csv",na.strings=c(""," ","NA"))
+date <- read.csv("")
+library(mlbench)
+library(caret)
+library(C50)
+library(partykit)
+library(gmodels)
+
+#-------------------
+# Data cleaning
+#-------------------
+#number of corr
+plot(table(date$int_corr))
+#dec_o versus attr_o
+plot(date$dec_o, date$attr_o)
+#Using a linear model we see that there is a correlation between the decision of the partner and the rating that was given
+lm <- glm(dec_o ~ attr_o, data=date, family = binomial)
+summary(lm)
+#Remove rows where columns 16:23 are null
+#Should have 8305 rows
+for(k in 1:2){
+  for(i in 1:nrow(date)){
+    for(j in 16:23){
+      if(is.na(date[i,j]) && j == 23){
+        date <- date[-i,]
+     } 
+      if(!is.na(date[i,j])) break
+    }
+  }
+}
+#Make mn_sat, tuition, income as numeric
+date$mn_sat <- as.numeric(as.character(date$mn_sat))
+date$tuition <- as.numeric(as.character(date$tuition))
+date$income <- as.numeric(as.character(date$income))
+#Decision as factor
+date$field_cd <- factor(date$field_cd)
+date$career_c <- factor(date$career_c)
+date$dec_o <- as.factor(date$dec_o)
+
+#Code similar schools to the same id: 
+for(i in 1:nrow(date)){
+  if(!is.na(date[i,c("undergra")])){
+    temp <- date[i,c("undergra")]
+    if(temp == "Brown University"){
+      #(Brown, Brown University)
+      temp <- "Brown"
+    }else if(temp == "ColumbiaU" || temp == "Columbia University" || 
+             temp == "Columbia College" || temp == "Columbia Business School"||
+             temp == "Columbia College, CU"){
+      #(Columbia, ColumbiaU, Columbia University, Columbia College, Columbia Business School, "Columbia College, CU")
+      temp <- "Columbia"
+      date[i,c("mn_sat")] <- 1430
+      date[i,c("tuition")] <- 26908
+    }else if(temp=="Conneticut College"){
+      #(Connecticut College, Conneticut College)
+      temp <- "Connecticut College"
+    }else if(temp=="Cornell University"){
+      #(Cornell, Cornell University)
+      temp <- "Cornell"
+    }else if(temp=="Ecole Polytechnique (France)"){
+      #(ecole polytechnique, Ecole Polytechnique (France) )
+      temp <- "ecole polytechnique"
+    }else if(temp=="Fudan University, Shanghai, China"){
+      #(Fudan, Fudan University, Shanghai, China)
+      temp <- "Fudan"
+    }else if(temp=="Georgetown University"){
+      #(Georgetown, Georgetown University)
+      temp <- "Georgetown"
+    }else if(temp=="Harvard University" || temp=="Harvard College"){
+      temp <- "Harvard"
+    }else if(temp=="Holy Cross College"){
+      #(Holy Cross, Holy Corss College)
+      temp <- "Holy Cross"
+    }else if(temp=="Loyola College in Maryland"){
+      #(Loyola, Loyola College in Maryland)
+      temp <- "Loyola"
+    }else if(temp=="nyu"){
+      #(New York University, nyu)
+      temp <- "New York University"
+    }else if(temp=="oberlin"){
+      #(Oberlin College, Oberlin)
+      temp <- "Oberlin College"
+    }else if(temp=="Oxford"){
+      #(Oxford University, Oxford)
+      temp <- "Oxford University"
+    }else if(temp=="Princeton U." || temp=="Princeton University"){
+      #(Princeton, Princeton U., Princeton University)
+      temp <- "Princeton"
+    }else if(temp=="Rice University"){
+      #(Rice, Rice University)
+      temp <- "Rice"
+      date[i,c("mn_sat")] <- 1410
+    }else if(temp=="Rutgers College" || temp=="Rutgers University - New Brunswick"){
+      temp <- "Rutgers University"
+    }else if(temp=="Stanford University"){
+      temp <- "Stanford"
+    }else if(temp=="UC, IRVINE!!!!!!!!!"){
+      temp <- "UC Irvine"
+    }else if(temp=="University of California at Santa Cruz"){
+      temp <- "UC Santa Cruz"
+    }else if(temp=="U of Michigan" || temp=="University of Michigan-Ann Arbor"){
+      temp <- "University of Michigan"
+    }else if(temp=="umass" || temp=="University of Massachusetts-Amherst"){
+      temp <- "University of Massachusetts-Amherst"
+      date[i,c("mn_sat")] <- 1050
+    }else if(temp=="U.C. Berkeley" || temp=="Cal Berkeley"){
+      temp <- "UC Berkeley"
+    }else if(temp=="California State University Los Angeles"){
+      temp <- "UCLA"
+    }else if(temp=="University of California at Santa Barbara"){
+      temp <- "UCSB"
+    }else if(temp=="UPenn" || temp == "Univ of Pennsylvania"){
+      temp <- "University of Pennsylvania"
+    }else if(temp=="University of Wisconsin-Madison"){
+      temp <- "UW Madison"
+    }else if(temp=="Washington U. in St. Louis" || 
+              temp=="washington university in st louis"){
+      temp <- "Washington University in St. Louis"
+    }else if(temp=="Yale University"){
+      temp <- "Yale"
+    }
+  date[i,c("undergra")] <- temp
+  }
+}
+date$undergra <- factor(date$undergra)
+date$undergra
+
+#Male and female data frames
+male <- date[which(date$gender==1),]
+female <- date[which(date$gender==0),]
+
+#-------------------
+# Model Creation for Career
+#-------------------
+#make a career data frame - where not undergra, field_cd, career_c is not na
+#attempted to create model with schools but unable to get a tree with enough predictions
+career <- male[which(!is.na(male$undergra)),]
+career <- career[which(!is.na(career$age_o)),]
+set.seed(12345)
+career_rand <- career[order(runif(2407)), ]
+#Split the data frames into 75%/25%
+career_train <- career_rand[1:1805, ]
+career_test  <- career_rand[1806:2407, ]
+#Create a C5.0 model
+career_model <- C5.0(career_train[,c("field_cd", "career_c", "age_o")],career_train$dec_o)
+plot(career_model)
+
+career_pred <- predict(career_model, career_test)
+#Cross tabulation of predicted versus actual classes
+CrossTable(career_test$dec_o, career_pred,
+           prop.chisq = FALSE, prop.c = FALSE, prop.r = FALSE,
+           dnn = c('actual default', 'predicted default'))
