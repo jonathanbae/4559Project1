65a474f0d8b37a97c282d47f82dc40ac4863c3cd
jonathanbae <jonathanbae7@gmail.com>
2016-10-29 21:04:54 -0400

    attributes update

--- Project1.R
+++ Project1.R
@@ -6,134 +6,92 @@ library(caret)
 library(C50)
 library(partykit)
 library(gmodels)
-#1
+
 #-------------------
 # Data cleaning
 #-------------------
-#number of corr
-plot(table(date$int_corr))
-#dec_o versus attr_o
-plot(date$dec_o, date$attr_o)
-#Using a linear model we see that there is a correlation between the decision of the partner and the rating that was given
-lm <- glm(dec_o ~ attr_o, data=date, family = binomial)
-summary(lm)
-#Remove rows where columns 16:23 are null
-#Should have 8305 rows
-for(k in 1:2){
-  for(i in 1:nrow(date)){
-    for(j in 16:23){
-      if(is.na(date[i,j]) && j == 23){
-        date <- date[-i,]
-     } 
-      if(!is.na(date[i,j])) break
-    }
-  }
-}
-#Make mn_sat, tuition, income as numeric
-date$mn_sat <- as.numeric(as.character(date$mn_sat))
-date$tuition <- as.numeric(as.character(date$tuition))
-date$income <- as.numeric(as.character(date$income))
-#Decision as factor
-date$field_cd <- factor(date$field_cd)
-date$career_c <- factor(date$career_c)
-date$dec_o <- as.factor(date$dec_o)
-
-#Code similar schools to the same id: 
-for(i in 1:nrow(date)){
-  if(!is.na(date[i,c("undergra")])){
-    temp <- date[i,c("undergra")]
-    if(temp == "Brown University"){
-      #(Brown, Brown University)
-      temp <- "Brown"
-    }else if(temp == "ColumbiaU" || temp == "Columbia University" || 
-             temp == "Columbia College" || temp == "Columbia Business School"||
-             temp == "Columbia College, CU"){
-      #(Columbia, ColumbiaU, Columbia University, Columbia College, Columbia Business School, "Columbia College, CU")
-      temp <- "Columbia"
-      date[i,c("mn_sat")] <- 1430
-      date[i,c("tuition")] <- 26908
-    }else if(temp=="Conneticut College"){
-      #(Connecticut College, Conneticut College)
-      temp <- "Connecticut College"
-    }else if(temp=="Cornell University"){
-      #(Cornell, Cornell University)
-      temp <- "Cornell"
-    }else if(temp=="Ecole Polytechnique (France)"){
-      #(ecole polytechnique, Ecole Polytechnique (France) )
-      temp <- "ecole polytechnique"
-    }else if(temp=="Fudan University, Shanghai, China"){
-      #(Fudan, Fudan University, Shanghai, China)
-      temp <- "Fudan"
-    }else if(temp=="Georgetown University"){
-      #(Georgetown, Georgetown University)
-      temp <- "Georgetown"
-    }else if(temp=="Harvard University" || temp=="Harvard College"){
-      temp <- "Harvard"
-    }else if(temp=="Holy Cross College"){
-      #(Holy Cross, Holy Corss College)
-      temp <- "Holy Cross"
-    }else if(temp=="Loyola College in Maryland"){
-      #(Loyola, Loyola College in Maryland)
-      temp <- "Loyola"
-    }else if(temp=="nyu"){
-      #(New York University, nyu)
-      temp <- "New York University"
-    }else if(temp=="oberlin"){
-      #(Oberlin College, Oberlin)
-      temp <- "Oberlin College"
-    }else if(temp=="Oxford"){
-      #(Oxford University, Oxford)
-      temp <- "Oxford University"
-    }else if(temp=="Princeton U." || temp=="Princeton University"){
-      #(Princeton, Princeton U., Princeton University)
-      temp <- "Princeton"
-    }else if(temp=="Rice University"){
-      #(Rice, Rice University)
-      temp <- "Rice"
-      date[i,c("mn_sat")] <- 1410
-    }else if(temp=="Rutgers College" || temp=="Rutgers University - New Brunswick"){
-      temp <- "Rutgers University"
-    }else if(temp=="Stanford University"){
-      temp <- "Stanford"
-    }else if(temp=="UC, IRVINE!!!!!!!!!"){
-      temp <- "UC Irvine"
-    }else if(temp=="University of California at Santa Cruz"){
-      temp <- "UC Santa Cruz"
-    }else if(temp=="U of Michigan" || temp=="University of Michigan-Ann Arbor"){
-      temp <- "University of Michigan"
-    }else if(temp=="umass" || temp=="University of Massachusetts-Amherst"){
-      temp <- "University of Massachusetts-Amherst"
-      date[i,c("mn_sat")] <- 1050
-    }else if(temp=="U.C. Berkeley" || temp=="Cal Berkeley"){
-      temp <- "UC Berkeley"
-    }else if(temp=="California State University Los Angeles"){
-      temp <- "UCLA"
-    }else if(temp=="University of California at Santa Barbara"){
-      temp <- "UCSB"
-    }else if(temp=="UPenn" || temp == "Univ of Pennsylvania"){
-      temp <- "University of Pennsylvania"
-    }else if(temp=="University of Wisconsin-Madison"){
-      temp <- "UW Madison"
-    }else if(temp=="Washington U. in St. Louis" || 
-              temp=="washington university in st louis"){
-      temp <- "Washington University in St. Louis"
-    }else if(temp=="Yale University"){
-      temp <- "Yale"
-    }
-  date[i,c("undergra")] <- temp
-  }
-}
-date$undergra <- factor(date$undergra)
-date$undergra
-
 #Male and female data frames
 male <- date[which(date$gender==1),]
 female <- date[which(date$gender==0),]
 
+#number of corr
+plot(table(male$int_corr)) 
+plot(table(female$int_corr))
+
+#Make mn_sat, tuition, income as numeric
+male$mn_sat <- as.numeric(as.character(male$mn_sat))
+male$tuition <- as.numeric(as.character(male$tuition))
+male$income <- as.numeric(as.character(male$income))
+#Decision as factor
+male$field_cd <- factor(male$field_cd)
+male$career_c <- factor(male$career_c)
+male$dec_o <- as.factor(male$dec_o)
+
+#Remove rows where columns 99:104 are null (attr, sinc, intel, fun, amb)
+#Should have 4099 rows
+for(x in 1:3){
+  for(i in 1:nrow(male)){
+    for(j in 99:103){
+      if(is.na(male[i,j]) && j == 103){
+        male <- male[-i,]
+     } 
+      if(!is.na(male[i,j])) break
+    }
+  }
+}
+#Get rows of speedmaler by iid
+attrMiss <- which(is.na(male$attr))
+sincMiss <- which(is.na(male$sinc))
+intelMiss <- which(is.na(male$intel))  
+funMiss <- which(is.na(male$fun))
+ambMiss <- which(is.na(male$amb))
+
+avera <- function(missing, num){
+  #Average attractive from other dates
+  for(i in missing){
+    #Get id of the first missing attractive
+    getiid <- male[i, 1]
+    #Store rows of values for the id
+    store <- which(getiid==male[,1])
+    numRate = 0
+    tot = 0
+    #Get average value from the other people
+    for(j in store){
+      if(!is.na(male[j,num]) ){
+        numRate = numRate + 1
+        tot = tot + male[j,num]
+      }
+    }
+    #store the average of the scores
+    male[i,num] <<- tot/numRate
+  }
+}
+avera(attrMiss, 99)
+avera(sincMiss, 100)
+avera(intelMiss, 101)
+avera(funMiss, 102)
+avera(ambMiss, 103)
+#Empty values for iid 465 for fun and intel; 187 for amb
+which(is.na(male$attr))
+which(is.na(male$sinc))
+intelMiss <- which(is.na(male$intel))  
+male[3376,1]
+funMiss <- which(is.na(male$fun))
+ambMiss <- which(is.na(male$amb))
+male[1228,1]
+#For arbitrary values, insert 5 into fun, intel and ambition
+for( i in intelMiss ){
+  male[i, 101] <- 5
+  male[i, 102] <- 5
+}
+for( i in ambMiss ){
+  male[i, 103] <- 5
+}
+
 #-------------------
 # Model Creation for Career
 #-------------------
-#make a career data frame - where not undergra, field_cd, career_c is not na
+#make a career data frame - where field_cd, career_c is not na
 #attempted to create model with schools but unable to get a tree with enough predictions
 career <- male[which(!is.na(male$undergra)),]
 career <- career[which(!is.na(career$age_o)),]
