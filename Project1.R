885bd75fc70ce0e57d6818a5dceef0b77fc9526a
jonathanbae <jonathanbae7@gmail.com>
2016-10-31 20:20:59 -0400

    Sentiment

--- Project1.R
+++ Project1.R
@@ -171,6 +171,242 @@ sapply(male, function(x) sum(is.na(x)))
 
 
 #-------------------
+#Sentiment Analysis
+#-------------------
+library(twitteR)
+library(base64enc)
+library(ROAuth)
+require(RCurl)
+library(stringr)
+library(ggmap)
+library(dplyr)
+library(RTextTools)
+library(plyr)
+library(tm)
+library(wordcloud)
+key='5VmEHYyBmVtY6nXjbnK9mu5uR'
+secret='8VcjEVcit7AGUBfh3B3Z13OiXHVwtuvAFWMaIgVWFBTnMUsuo6'
+setwd("~/School/Fall 2016/DS 4559/text_mining_and_web_scraping/")
+access_token='788199063812399104-8L8mRuzn1Mn8Ld9WMVlCuPbJD0Mk4pH'
+access_token_secret='0fZm0jViHzVnn5UI5T5w3RGgbhIjmTgZ6qiAPy1jf7dpM'
+
+download.file(url="http://curl.haxx.se/ca/cacert.pem",
+              destfile="~/School/Fall 2016/DS 4559/text_mining_and_web_scraping/cacert.pem",
+              method="curl")
+authenticate <- OAuthFactory$new(consumerKey=key,
+                                 consumerSecret=secret,
+                                 requestURL="https://api.twitter.com/oauth/request_token",
+                                 accessURL="https://api.twitter.com/oauth/access_token",
+                                 authURL="https://api.twitter.com/oauth/authorize")
+setup_twitter_oauth(key, secret, access_token, access_token_secret)
+save(authenticate, file="twitter authentication.Rdata")
+# Speed Dating: word clouds created for speed dating, love, romance
+#Geocode for Charlottesville: 38.0293,78.4767
+#Geocode for Manhattan 40.7831, 73.9712
+some_tweets = searchTwitter("romance", n=2000, lang="en",since ='2000-01-01',
+                            until ='2016-10-31',  retryOnRateLimit=120, geocode='38.0293,78.4767,500mi')
+# get the text
+some_txt = sapply(some_tweets, function(x) x$getText())
+# remove retweet entities
+some_txt = gsub("(RT|via)((?:\\b\\W*@\\w+)+)", "", some_txt)
+# remove at people
+some_txt = gsub("@\\w+", "", some_txt)
+# remove punctuation
+some_txt = gsub("[[:punct:]]", "", some_txt)
+# remove numbers
+some_txt = gsub("[[:digit:]]", "", some_txt)
+# remove html links
+some_txt = gsub("http\\w+", "", some_txt)
+# remove unnecessary spaces
+some_txt = gsub("[ \t]{2,}", "", some_txt)
+some_txt = gsub("^\\s+|\\s+$", "", some_txt)
+# define "tolower error handling" function 
+try.error = function(x)
+{
+  # create missing value
+  y = NA
+  # tryCatch error
+  try_error = tryCatch(tolower(x), error=function(e) e)
+  # if not an error
+  if (!inherits(try_error, "error"))
+    y = tolower(x)
+  # result
+  return(y)
+}
+# lower case using try.error with sapply 
+some_txt = sapply(some_txt, try.error)
+# remove NAs in some_txt
+some_txt = some_txt[!is.na(some_txt)]
+names(some_txt) = NULL
+
+col=brewer.pal(6,"Dark2")
+wordcloud(some_txt, min.freq=20, scale=c(5,2),rot.per = 0.25,
+          random.color=T, max.word=50, random.order=F,colors=col)
+
+positives= readLines("~/School/Fall 2016/DS 4559/R/positive_words.txt")
+negatives= readLines("~/School/Fall 2016/DS 4559/R/negative_words.txt")
+
+score.sentiment = function(sentences, pos.words, neg.words, .progress='none')
+{
+  require(plyr)
+  require(stringr)
+  scores = laply(sentences, function(sentence, pos.words, neg.words) {
+    sentence = gsub('[[:punct:]]', '', sentence)
+    sentence = gsub('[[:cntrl:]]', '', sentence)
+    sentence = gsub('\\d+', '', sentence)
+    sentence = tolower(sentence)
+    word.list = str_split(sentence, '\\s+')
+    words = unlist(word.list)
+    pos.matches = match(words, pos.words)
+    neg.matches = match(words, neg.words)
+    pos.matches = !is.na(pos.matches)
+    neg.matches = !is.na(neg.matches)
+    score = sum(pos.matches) - sum(neg.matches)
+    return(score)
+  }, pos.words, neg.words, .progress=.progress )
+  scores.df = data.frame(score=scores, text=sentences)
+  return(scores.df)
+}
+
+Score <- score.sentiment(some_txt,positives,negatives,.progress='none')
+head(Score)
+hist(Score$score,xlab="Sentiment Score ",main="Sentiment of sample tweets that have Trump in them ",
+     border="black",col="skyblue")
+sum(Score$score)
+
+classify_emotion <- function(textColumns,algorithm="bayes",prior=1.0,verbose=FALSE,...) {
+  matrix <- create_matrix(textColumns,...)
+  lexicon <- read.csv("~/School/Fall 2016/DS 4559/R/emotions.csv.gz",header=FALSE)
+  counts <- list(anger=length(which(lexicon[,2]=="anger")),disgust=length(which(lexicon[,2]=="disgust")),fear=length(which(lexicon[,2]=="fear")),joy=length(which(lexicon[,2]=="joy")),sadness=length(which(lexicon[,2]=="sadness")),surprise=length(which(lexicon[,2]=="surprise")),total=nrow(lexicon))
+  documents <- c()
+  for (i in 1:nrow(matrix)) {
+    if (verbose) print(paste("DOCUMENT",i))
+    scores <- list(anger=0,disgust=0,fear=0,joy=0,sadness=0,surprise=0)
+    doc <- matrix[i,]
+    words <- findFreqTerms(doc,lowfreq=1)
+    for (word in words) {
+      for (key in names(scores)) {
+        emotions <- lexicon[which(lexicon[,2]==key),]
+        index <- pmatch(word,emotions[,1],nomatch=0)
+        if (index > 0) {
+          entry <- emotions[index,]
+          category <- as.character(entry[[2]])
+          count <- counts[[category]]
+          score <- 1.0
+          if (algorithm=="bayes") score <- abs(log(score*prior/count))
+          if (verbose) {
+            print(paste("WORD:",word,"CAT:",category,"SCORE:",score))
+          }
+          scores[[category]] <- scores[[category]]+score
+        }
+      }
+    }
+    if (algorithm=="bayes") {
+      for (key in names(scores)) {
+        count <- counts[[key]]
+        total <- counts[["total"]]
+        score <- abs(log(count/total))
+        scores[[key]] <- scores[[key]]+score
+      }
+    } else {
+      for (key in names(scores)) {
+        scores[[key]] <- scores[[key]]+0.000001
+      }
+    }
+    best_fit <- names(scores)[which.max(unlist(scores))]
+    if (best_fit == "disgust" && as.numeric(unlist(scores[2]))-3.09234 < .01) best_fit <- NA
+    documents <- rbind(documents,c(scores$anger,scores$disgust,scores$fear,scores$joy,scores$sadness,scores$surprise,best_fit))
+  }
+  colnames(documents) <- c("ANGER","DISGUST","FEAR","JOY","SADNESS","SURPRISE","BEST_FIT")
+  return(documents)
+}
+
+classify_polarity <- function(textColumns,algorithm="bayes",pstrong=0.5,pweak=1.0,prior=1.0,verbose=FALSE,...) {
+  matrix <- create_matrix(textColumns,...)
+  lexicon <- read.csv("~/School/Fall 2016/DS 4559/R/subjectivity.csv.gz",header=FALSE)
+  counts <- list(positive=length(which(lexicon[,3]=="positive")),negative=length(which(lexicon[,3]=="negative")),total=nrow(lexicon))
+  documents <- c()
+  for (i in 1:nrow(matrix)) {
+    if (verbose) print(paste("DOCUMENT",i))
+    scores <- list(positive=0,negative=0)
+    doc <- matrix[i,]
+    words <- findFreqTerms(doc,lowfreq=1)
+    for (word in words) {
+      index <- pmatch(word,lexicon[,1],nomatch=0)
+      if (index > 0) {
+        entry <- lexicon[index,]
+        polarity <- as.character(entry[[2]])
+        category <- as.character(entry[[3]])
+        count <- counts[[category]]
+        score <- pweak
+        if (polarity == "strongsubj") score <- pstrong
+        if (algorithm=="bayes") score <- abs(log(score*prior/count))
+        if (verbose) {
+          print(paste("WORD:",word,"CAT:",category,"POL:",polarity,"SCORE:",score))
+        }
+        scores[[category]] <- scores[[category]]+score
+      }		
+    }
+    
+    if (algorithm=="bayes") {
+      for (key in names(scores)) {
+        count <- counts[[key]]
+        total <- counts[["total"]]
+        score <- abs(log(count/total))
+        scores[[key]] <- scores[[key]]+score
+      }
+    } else {
+      for (key in names(scores)) {
+        scores[[key]] <- scores[[key]]+0.000001
+      }
+    }
+    best_fit <- names(scores)[which.max(unlist(scores))]
+    ratio <- as.integer(abs(scores$positive/scores$negative))
+    if (ratio==1) best_fit <- "neutral"
+    documents <- rbind(documents,c(scores$positive,scores$negative,abs(scores$positive/scores$negative),best_fit))
+    if (verbose) {
+      print(paste("POS:",scores$positive,"NEG:",scores$negative,"RATIO:",abs(scores$positive/scores$negative)))
+      cat("\n")
+    }
+  }
+  colnames(documents) <- c("POS","NEG","POS/NEG","BEST_FIT")
+  return(documents)
+}
+
+# classify emotion
+class_emo = classify_emotion(some_txt, algorithm="bayes", prior=1.0)
+# get emotion best fit
+emotion = class_emo[,7]
+# substitute NA's by "unknown"
+emotion[is.na(emotion)] = "unknown"
+
+# classify polarity
+class_pol = classify_polarity(some_txt, algorithm="bayes")
+# get polarity best fit
+polarity = class_pol[,4]
+
+# data frame with results
+sent_df = data.frame(text=some_txt, emotion=emotion,
+                     polarity=polarity, stringsAsFactors=FALSE)
+
+# sort data frame
+sent_df = within(sent_df,
+                 emotion <- factor(emotion, levels=names(sort(table(emotion), decreasing=TRUE))))
+
+# plot distribution of emotions
+library(ggplot2)
+ggplot(sent_df, aes(x=emotion)) +
+  geom_bar(aes(y=..count.., fill=emotion)) +
+  scale_fill_brewer(palette="Dark2") +
+  labs(x="emotion categories", y="number of tweets")
+
+ggplot(sent_df, aes(x=polarity)) +
+  geom_bar(aes(y=..count.., fill=polarity))
+
+text.corpus <- Corpus(VectorSource(some_txt))
+dtm <- DocumentTermMatrix(text.corpus)
+
+#-------------------
 # Model Creation for Male
 #-------------------
 #Attributes: age, field_cd, race, (income), goal, date, go_out, career_c, activities, exhappy, attr, sinc
